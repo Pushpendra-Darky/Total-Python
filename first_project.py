daaa54f3d8b01b7349ca32afd40771ac03670dc9
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2023-11-09 19:29:08 +0530

    first project

---
+++ first_project.py
@@ -0,0 +1,13 @@
+# hello world
+print("Hello World I wanna say 'Thank You'")
+
+# String
+print('My Name Is Push' + 'pendra' +' '+ "Singh")
+print("100 + 50 =")
+
+# arith calc
+print(100 + 50)
+
+# input
+print("Your name is " + input("Tell me Your Name:"))
+print("Your last name is " + input("Tell me your last name:"))
